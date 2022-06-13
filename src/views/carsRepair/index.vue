6571febe3659ea0a1a910444db7035a1fdd17713
seven.si <Seven.Si@gtechdigi.com>
2022-06-13 18:52:28 +0800

    tt

--- src/views/carsRepair/index.vue
+++ src/views/carsRepair/index.vue
@@ -3,48 +3,60 @@
     <div class="app-header">
       <el-input v-model="queryParams.userName" placeholder="姓名" clearable style="width: 200px;margin: 5px;"
                 class="filter-item"
-                @keyup.enter.native="handleFilter" @clear="getList"/>
+                @keyup.enter.native="getList"
+                @clear="getList"
+                @input="getList"/>
       <el-input v-model="queryParams.phone" placeholder="手机号" clearable style="width: 200px;margin: 5px;"
                 class="filter-item"
-                @keyup.enter.native="handleFilter" @clear="getList"/>
+                @input="getList"
+                @keyup.enter.native="getList"
+                @clear="getList"/>
       <el-input v-model="queryParams.carNumber" placeholder="车牌号" clearable style="width: 200px;margin: 5px;"
                 class="filter-item"
-                @keyup.enter.native="handleFilter" @clear="getList"/>
-      <el-date-picker v-model="queryParams.createTimeArray" type="daterange" align="right" unlink-panels range-separator="至"
-                      start-placeholder="开始日期" end-placeholder="结束日期" :picker-options="pickerOptions">
-      </el-date-picker>
-      <el-button class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+                @input="getList"
+                @keyup.enter.native="getList"
+                @clear="getList"/>
+      <el-date-picker v-model="queryParams.startCreateTime" align="right" style="width: 200px;margin: 5px;" type="date"
+                      placeholder="开始日期" :picker-options="pickerOptions"></el-date-picker>
+      <el-date-picker v-model="queryParams.endCreateTime" align="right" style="width: 200px;margin: 5px;" type="date"
+                      placeholder="结束日期" :picker-options="pickerOptions"></el-date-picker>
+      <el-button class="filter-item" style="margin: 5px;" type="primary" icon="el-icon-search" @click="getList">
         {{ $t('table.search') }}
       </el-button>
-      <el-button class="filter-item" type="primary" icon="el-icon-edit"
-                 @click="addCarsRepair">
+      <el-button class="filter-item" style="margin: 5px;" type="primary" icon="el-icon-edit" @click="addCarsRepair">
         {{ $t('table.add') }}
       </el-button>
     </div>
     <div class="app-body">
-      <el-table :data="list" height="500" border style="width: 100%">
+      <el-table :data="list" fit stripe border highlight-current-row style="width: 100%">
         <el-table-column prop="userName" label="姓名" width="180" align="center"></el-table-column>
         <el-table-column prop="phone" label="手机号" width="180" align="center"></el-table-column>
         <el-table-column prop="carNumber" label="车牌号" width="180" align="center"></el-table-column>
         <el-table-column prop="carsRepairType" label="维修类型" width="180" align="center"></el-table-column>
         <el-table-column prop="carsRepairText" label="维修内容" width="180" align="center"></el-table-column>
         <el-table-column prop="createTime" label="维修时间" width="240" align="center" sortable></el-table-column>
-
-        <el-table-column :label="$t('table.actions')" align="center" min-width="230" class-name="small-padding fixed-width">
+        <el-table-column :label="$t('table.actions')"
+                         align="center"
+                         min-width="230"
+                         class-name="small-padding fixed-width">
           <template slot-scope="scope">
-            <el-button type="primary" size="mini" style="min-width: 40px" @click="updateCarsRepair(scope.row)">编辑</el-button>
-            <el-button type="danger" size="mini" style="min-width: 40px" @click="deleteCarsRepair(scope.row)">删除</el-button>
+            <el-button type="primary" size="mini" style="min-width: 40px" @click="updateCarsRepair(scope.row)">编辑
+            </el-button>
+            <el-button type="danger" size="mini" style="min-width: 40px" @click="deleteCarsRepair(scope.row)">删除
+            </el-button>
           </template>
         </el-table-column>
       </el-table>
     </div>
     <div class="app-footer">
-      <pagination
+      <el-pagination
         v-show="total>0"
         :total="total"
-        :page.sync="queryParams.pageNum"
-        :limit.sync="queryParams.pageSize"
-        @pagination="getList"/>
+        layout="total, sizes, prev, pager, next, jumper"
+        :page-count="queryParams.pageNum"
+        :page-size="queryParams.pageSize"
+        @current-change="getListByNumber"
+        @size-change="getListByPage"/>
     </div>
   </div>
 </template>
@@ -57,36 +69,15 @@ export default {
     return {
       list: [],
       total: 0,
-      queryParams: {pageNum: 1, pageSize: 10, userName: null, phone: null, carNumber: null, createTimeArray: this.createTimeArray},
-      // queryParams: {pageNum: 1, pageSize: 10},
-      pickerOptions: {
-        shortcuts: [{
-          text: '最近一周',
-          onClick(picker) {
-            const end = new Date();
-            const start = new Date();
-            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-            picker.$emit('pick', [start, end]);
-          }
-        }, {
-          text: '最近一个月',
-          onClick(picker) {
-            const end = new Date();
-            const start = new Date();
-            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-            picker.$emit('pick', [start, end]);
-          }
-        }, {
-          text: '最近三个月',
-          onClick(picker) {
-            const end = new Date();
-            const start = new Date();
-            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
-            picker.$emit('pick', [start, end]);
-          }
-        }]
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        userName: null,
+        phone: null,
+        carNumber: null,
+        startCreateTime: null,
+        endCreateTime: null
       },
-      createTimeArray: [],
       request_body: {
         username: null,
         phone: null,
@@ -104,6 +95,20 @@ export default {
       this.getList()
     },
     getList() {
+      queryAllCarsRepair(this.queryParams).then(res => {
+        this.list = res.data.list
+        this.total = res.data.total
+      })
+    },
+    getListByPage(size) {
+      this.queryParams.pageSize = size
+      queryAllCarsRepair(this.queryParams).then(res => {
+        this.list = res.data.list
+        this.total = res.data.total
+      })
+    },
+    getListByNumber(number) {
+      this.queryParams.pageNum = number
       queryAllCarsRepair(this.queryParams).then(res => {
         this.list = res.data.list
         this.total = res.data.total
@@ -131,5 +136,11 @@ export default {
 </script>
 
 <style>
+.app-body{
+  margin-top: 20px;
+}
+.app-footer {
+  margin-top: 50px;
+}
 
 </style>
